6edfa6ab11756daa7f749a27203d6dc743e9499c
Julian_Ehiem <jules.ehiem@gmail.com>
2020-07-19 09:27:37 -0400

    added modal

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // create an array of image sources;
 "use strict";
 
+const navToggle = document.querySelector(".menu-button");
+const nav = document.querySelector("nav");
+const containerAll = document.querySelector(".container-all");
+
+navToggle.addEventListener("click", () => {
+  containerAll.style.transition = "transform 250ms ease-in-out";
+  document.body.classList.toggle("nav-is-open");
+});
+
+nav.addEventListener("click", () => {
+  containerAll.style.transition = "0ms";
+  document.body.classList.remove("nav-is-open");
+});
+
 let images = [
   "img/web-apps-with-ajax-500x386.png",
   "img/ajax-and-fetch-500x386.png",
