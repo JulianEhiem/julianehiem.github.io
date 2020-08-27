a95f13083bd458a02d1e608bf8d88197bf28fb75
Julian_Ehiem <jules.ehiem@gmail.com>
2020-08-27 16:07:07 -0400

    added modal page to first project

--- modal.js
+++ modal.js
@@ -1,25 +1,37 @@
-const portfolioContainer = document.querySelector(".projects");
+// const portfolioContainer = document.querySelector(".projects");
 
-portfolioContainer.addEventListener("click", (e) => {
-  //console.log(e);
-  e.preventDefault();
+// portfolioContainer.addEventListener("click", (e) => {
+//   //console.log(e);
+//   e.preventDefault();
 
-  const modalToggle = e.target.closest(".project-link");
-  console.log(modalToggle);
-  if (!modalToggle) return;
+//   const modalToggle = e.target.closest(".project-link");
+//   console.log(modalToggle);
+//   if (!modalToggle) return;
 
-  const modal = modalToggle.parentNode.nextElementSibling;
-  const closeButton = modal.querySelector(".modal-close");
+//   const modal = modalToggle.parentNode.nextElementSibling;
+//   const closeButton = modal.querySelector(".modal-close");
 
-  const modalOpen = () => {
-    modal.classList.add("is-open");
-    document.body.style.overflowY = "hidden";
-  };
-  const modalClose = () => {
-    modal.classList.remove("is-open");
-    document.body.style.overflowY = "scroll";
-  };
-  closeButton.addEventListener("click", modalClose);
+//   const modalOpen = () => {
+//     modal.classList.add("is-open");
+//     document.body.style.overflowY = "hidden";
+//   };
+//   const modalClose = () => {
+//     modal.classList.remove("is-open");
+//     document.body.style.overflowY = "scroll";
+//   };
+//   closeButton.addEventListener("click", modalClose);
 
-  modalOpen();
+//   modalOpen();
+// });
+
+const open = document.querySelector("#open");
+const modalContainer = document.querySelector("#modalContainer");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
 });
